0b57383866ab3ffbf0b8774cfa3b7ccd44e6eeaf
Cosmo Swarbreck <c.swarbreck@gmail.com>
2018-04-16 18:21:27 +0100

    practice using handlebars

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const handlebars = require('handlebars');
+const hbs = require ('hbs');
 const app = express();
 
+
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res)=>{
+hbs.registerPartials(__dirname + '/views/partials');
 
-    res.send('Hello Express!');
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+});
+
+// You can register as many helpers as you like:
+
+hbs.registerHelper('screamIt', (text)=>{
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res)=>{
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'are you having a good time?',
+        bodyContent: new Date(),
+        
+    });
+});
+
+app.get('/about', (req, res)=>{
+
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        
+    });
 });
 
 app.listen(3000, ()=>{
